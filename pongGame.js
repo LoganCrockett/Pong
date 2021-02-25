c1a269becf68bf5d4342ee2f3ac9514dbf87ffa8
Logan-Crockett <69995104+Logan-Crockett@users.noreply.github.com>
2021-02-25 15:20:34 -0500

    Added Scoreboard & Score Win Condition
    Updated Initialize() to reset the board after each point is score. Fixes the double puck issue

--- pongGame.js
+++ pongGame.js
@@ -7,8 +7,9 @@ let canvas, ctx;
 let keysPressed = {};
 
 // Flags to end the game
-let player1Win = false;
-let player2Win = false;
+// let player1Win = false;
+// let player2Win = false;
+let won = false;
 
 // These three variables will keep track of each objects postion
 var player1 = {
@@ -16,6 +17,7 @@ var player1 = {
     y: 10,
     width: 5,
     height: 60,
+    score: 0,
 };
 
 var player2 = {
@@ -23,6 +25,7 @@ var player2 = {
     y: 10,
     width: 5,
     height: 60,
+    score: 0,
 }
 
 var puck = {
@@ -68,17 +71,40 @@ function init() {
 }
 
 /*
- * This will intialize the game board
+ * This will intialize the game board for Game & round Start
  */
 function initialize() {
+    // Since this will initialize the board for game & round start, we
+    // need to clear the canvas
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+
     // Start by drawing player one
-    drawPlayer1(10, 10, 5, 60);
+    // This will draw the player either in its default position, or whatever its current position is
+    // drawPlayer1(10, 10, 5, 60);
+    drawPlayer1(player1.x, player1.y, player1.width, player1.height);
 
     // Now Draw player two
-    drawPlayer2(580, 10, 5, 60);
+    // drawPlayer2(580, 10, 5, 60);
+    drawPlayer2(player2.x, player2.y, player2.width, player2.height);
 
     // Draw the Puck
-    drawPuck(300, 175, 10, 0, 360);
+    // drawPuck(300, 175, 10, 0, 360);
+
+    // Move the puck to its starting position before drawing
+    movePuckToStart();
+    drawPuck(puck.x, puck.y, puck.r, puck.start, puck.end);
+
+    /**
+     * Basically, when reseting the board, wait 1 second before giving the puck velocity
+     * This will allow the players to continue moving once the puck scores
+     */
+    setTimeout(() => {
+        puck.xVelocity = 2;
+        puck.yVelocity = 1.5;
+    }, 1000)
+
+    // Reset our flag
+    won = false;
 
     // Call our gameLoop to start the game
     gameLoop();
@@ -111,19 +137,39 @@ function drawPuck(x, y, r, start, end) {
     return;
 }
 
+/**
+ * This updates the positon of the puck back to where it started
+ * It also sets its x & y velocity to 0
+ * The initialize() function will give it velocity
+ */
+function movePuckToStart() {
+    puck = {
+        x: 300,
+        y: 175,
+        r: 10,
+        start: 0,
+        end: 360,
+    
+        // For velocity of the puck
+        xVelocity: 0,
+        yVelocity: 0
+    }
+    return
+}
+
 function checkAndHandlePlayerCollison() {
     // Player 1
 
     /**
      * In essence, check if:
      * 1: The Puck x value (minus radius) is less than the player x value (plus the player width)
-     * 2: The Puck y value is between the y & y+height values of the player
+     * 2: The Puck y value (+- radius) is between the y & y+height values of the player
      */
 
     // Check for the x portion & see if it lines up
     if (puck.x - puck.r < player1.x + player1.width
         // Check if the y portions match up
-        && puck.y > player1.y && puck.y < player1.y + player1.height) {
+        && puck.y + puck.r > player1.y && puck.y - puck.r < player1.y + player1.height) {
             // If so, we have a collision, and need to change the velocity
         puck.xVelocity *= -1; // Should Flip the sign of the velocity
         puck.yVelocity *= -1;
@@ -134,11 +180,11 @@ function checkAndHandlePlayerCollison() {
     /**
      * In essence,check if
      * 1: the puck x value ( plus the radius) is greater than player x value
-     * 2: the puck y value is bewteen y & y+height values of the player
+     * 2: the puck y value (+- radius) is bewteen y & y+height values of the player
      */
 
     if (puck.x + puck.r > player2.x
-        && puck.y > player2.y && puck.y < player2.y + player2.height) {
+        && puck.y + puck.r > player2.y && puck.y - puck.r < player2.y + player2.height) {
             puck.xVelocity *= -1; // Should flip the sign of the velocity
             puck.yVelocity *= -1;
         }
@@ -147,11 +193,17 @@ function checkAndHandlePlayerCollison() {
 function checkForWinner() {
     // If it hits the left wall, player 2 wins
     if (puck.x < 0) {
-        player2Win = true;
+        // player2Win = true;
+        won = true;
+        player2.score += 1;
+        document.getElementById("p2score").innerHTML = player2.score;
     }
     // If it hits the right wall, player 1 wins
     if (puck.x > canvas.width) {
-        player1Win = true;
+        // player1Win = true;
+        won = true;
+        player1.score += 1;
+        document.getElementById("p1score").innerHTML = player1.score;
     }
 }
 
@@ -189,6 +241,7 @@ function gameLoop() {
     }
 
     // Step 3: Redraw our Player's in the new locations
+
     drawPlayer1(player1.x, player1.y, player1.width, player1.height)
     drawPlayer2(player2.x, player2.y, player2.width, player2.height)
 
@@ -219,19 +272,27 @@ function gameLoop() {
     checkForWinner();
 
     // While neither player has won
-    if (!player1Win && !player2Win) {
+    // if (!player1Win && !player2Win) {
+    if (!won) {
         //Request another animation frame
         window.requestAnimationFrame(gameLoop)
     }
     else {
         // Otherwise, we can cancel the animation frame, and display a winner
-        window.cancelAnimationFrame(gameLoop)
+        // .cancelAnimationFrame(gameLoop)
 
-        if (player1Win) {
-            window.alert("Player 1 Wins")
+        // If one of the players has a score equal to 10, declare a winner
+        if (player1.score === 10) {
+            window.alert("Player 1 Wins!!")
+            window.cancelAnimationFrame(gameLoop)
         }
+        else if (player2.score === 10) {
+            window.alert("Player 2 Wins!!")
+            window.cancelAnimationFrame(gameLoop)
+        }
+        // Otherwise, reset the board, and play again
         else {
-            window.alert("Player 2 Wins")
+            initialize();
         }
     }
 
